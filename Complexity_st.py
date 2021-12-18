cb753c646dc598235764a4a56d6213764df95e13
rbhopker <65057484+rbhopker@users.noreply.github.com>
2021-12-17 23:24:19 -0500

    Update Complexity_st.py

--- Complexity_st.py
+++ Complexity_st.py
@@ -34,7 +34,7 @@ conn = connect(credentials=credentials)
 # Perform SQL query on the Google Sheet.
 # Uses st.cache to only rerun when the query changes or after 10 min.
 def run_query(query):
-    rows = conn.execute(query,headers=0)
+    rows = conn.execute(query,headers=1)
     return rows
 
 sheet_url = st.secrets["private_gsheets_url"]
@@ -46,7 +46,7 @@ df.columns = rows.keys()
 st.write(df)
 # Print results.
 for row in rows:
-    st.markdown(f"{row}")
+    st.markdown(f"{row.test_id}")
 
 
 
